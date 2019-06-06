195e7b02ec88faeac2e7d954354ae39f67c0dc32
Tanima Ranjan <tanimaranjan@gmail.com>
2019-06-06 12:04:59 -0400

    Index JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-const puzzleEl = document.querySelector('#puzzle')
-const quessesEl = document.querySelector('#guesses')
+import Hangman from './hangman'
+import getPuzzle from './request'
 
-let game1
+const puzzleEl = document.querySelector('#puzzle')
+const guessEl = document.querySelector('#guesses')
+let game1 
 
 window.addEventListener('keypress', (e) => {
-    const guess = String.fromCharCode(e.charCode)
-    game1.makeGuess(guess)
+  game1.makeGuess(e.key)
+  render()
 })
+
+const render = () => {
+  puzzleEl.innerHTML = ''
+  guessEl.textContent = game1.statusMessage 
+  game1.puzzle.split('').forEach((letter) => {
+    const letterEl = document.createElement('span')
+    letterEl.textContent = letter
+    puzzleEl.appendChild(letterEl)
+  })
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('2')
+  game1 = new Hangman(puzzle, 8)
+  render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
